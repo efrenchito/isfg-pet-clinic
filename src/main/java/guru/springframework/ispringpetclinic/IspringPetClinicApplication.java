92ecf2bd9e10d8d9dc806e236b8fa89f938284e7
efren.diaz <efrenchito@gmail.com>
2021-03-09 11:57:06 -0500

    Initial commit

---
+++ src/main/java/guru/springframework/ispringpetclinic/IspringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.ispringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IspringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IspringPetClinicApplication.class, args);
+	}
+
+}
